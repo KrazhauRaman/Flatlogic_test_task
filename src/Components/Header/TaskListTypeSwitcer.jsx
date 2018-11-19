f4b1de9f9a7fed487b4992e685afc00fbff041d1
KrazhauRaman <krazhau.raman@gmail.com>
2018-11-19 05:09:20 +0300

    v 1.1
    Fixes for gh-pages

--- src/Components/Header/TaskListTypeSwitcer.jsx
+++ src/Components/Header/TaskListTypeSwitcer.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { HashRouter, Link } from 'react-router-dom';
 
 const TaskListTypeSwitcer = ({ pageSwitchCallBack }) =>
-  <nav>
+  <nav> {console.log(document.location.hash)}
     <HashRouter>
       <div className="btn-group" role="group" aria-label="Basic example">
         <Link to="/active-tasks">
-          <button type="button" className="btn btn-warning" onClick={pageSwitchCallBack} disabled={"#/active-tasks" === document.location.hash}>Active</button>
+          <button type="button" className="btn btn-warning" onClick={pageSwitchCallBack}
+            disabled={"#/active-tasks" === document.location.hash || "" === document.location.hash}>Active</button>
         </Link>
         <Link to="/completed-tasks">
-          <button type="button" className="btn btn-warning" onClick={pageSwitchCallBack} disabled={"#/completed-tasks" === document.location.hash}>Completed</button>
+          <button type="button" className="btn btn-warning" onClick={pageSwitchCallBack}
+            disabled={"#/completed-tasks" === document.location.hash}>Completed</button>
         </Link>
       </div>
     </HashRouter>
