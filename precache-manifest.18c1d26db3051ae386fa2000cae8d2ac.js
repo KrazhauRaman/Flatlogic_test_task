c1f5f9f56cb7dae95bc7fd9b305c32118e3eb43a
KrazhauRaman <krazhau.raman@gmail.com>
2018-11-19 05:01:10 +0300

    Updates

---
+++ precache-manifest.18c1d26db3051ae386fa2000cae8d2ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13a5ac0187511bf33e15",
+    "url": "/flatlogic_test_task/static/css/main.8d48f4b0.chunk.css"
+  },
+  {
+    "revision": "13a5ac0187511bf33e15",
+    "url": "/flatlogic_test_task/static/js/main.13a5ac01.chunk.js"
+  },
+  {
+    "revision": "1d770af89be7d3f04302",
+    "url": "/flatlogic_test_task/static/js/1.1d770af8.chunk.js"
+  },
+  {
+    "revision": "1f653fdd5390173749c5",
+    "url": "/flatlogic_test_task/static/js/runtime~main.1f653fdd.js"
+  },
+  {
+    "revision": "5877ae2d9387eed5397f5e7c0abfaaf6",
+    "url": "/flatlogic_test_task/index.html"
+  }
+];
